26a9368c8d67eadfd75a9d921ff01f7651c57a27
Holic <holic@HolicdeMacBook-Pro.local>
2025-12-07 12:40:39 +0900

    github test3

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven + Jenkins!");
-        System.out.println("Hello trigger test 2!");
+        System.out.println("Hello trigger test 3!");
     }
 }
